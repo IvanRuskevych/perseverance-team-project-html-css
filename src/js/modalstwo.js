7255cc4b4b08f6a9c19073566ce0444ce2509532
Ivan Ruskevych <ivan.ruskevych@gmail.com>
2022-12-23 21:11:53 +0200

    2022-12-23 21:11

---
+++ src/js/modalstwo.js
@@ -0,0 +1,15 @@
+// script for modal windows
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector('[data-modal-open-buy]'),
+    closeModalBtn: document.querySelector('[data-modal-close-buy]'),
+    modal: document.querySelector('[data-modal-buy]'),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden-buy');
+  }
+})();
