12087d23d5332b5f977ff42697412f0ea1a2716d
ivanruskevych <114290317+ivanruskevych@users.noreply.github.com>
2022-12-25 12:54:49 +0000

    Deploying to gh-pages from @ ivanruskevych/perseverance-team-project-html-css@9a9ca28ef501c2dfa15fdccd27ba17b4438cd57b 🚀

---
+++ index.1857ea5e.js
@@ -0,0 +1,2 @@
+!function(){var e=0;!function t(){var l,n=document.getElementsByClassName("mySlides");document.getElementsByClassName("dot");for(l=0;l<n.length;l++)n[l].style.display="none";++e>n.length&&(e=1);n[e-1].style.display="block",setTimeout(t,2e3)}()}();
+//# sourceMappingURL=index.1857ea5e.js.map
---
+++ index.5f563469.js
@@ -0,0 +1,2 @@
+var e=0;!function t(){var l,s=document.getElementsByClassName("mySlides");document.getElementsByClassName("dot");for(l=0;l<s.length;l++)s[l].style.display="none";++e>s.length&&(e=1);s[e-1].style.display="block",setTimeout(t,2e3)}();
+//# sourceMappingURL=index.5f563469.js.map
