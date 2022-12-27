c23205f5113fcf9c2e8f17b9980d7cce7933836e
Lisiuk-Ruslan <114481433+Lisiuk-Ruslan@users.noreply.github.com>
2022-12-27 22:29:47 +0200

    Зміни до слайдера

--- src/js/slideshow.js
+++ src/js/slideshow.js
@@ -1,21 +1,11 @@
-var slideIndex = 0;
-showSlides();
-function showSlides() {
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.querySelectorAll(".dot");
-  dots[0].classList.add('active');
-  for (let i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) { slideIndex = 1 }
-  slides[slideIndex - 1].style.display = "block";
-  for (let i = 0; i < dots.length; i++) {
-    if (i === slideIndex - 1) {
-      dots[i].classList.add('active');
-    } else {
-      dots[i].classList.remove('active');
-    }
-  }
-  setTimeout(showSlides, 2000); // Change image every 2 seconds
-}
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+  speed: 400,
+  spaceBetween: 100,
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+});
