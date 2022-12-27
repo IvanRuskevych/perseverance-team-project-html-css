57891f0a694438c1e86942e05e502ff5b169f8f8
Igor Stovpyra <111482726+Asgiliot@users.noreply.github.com>
2022-12-27 20:28:07 +0200

    fix js

--- src/index.js
+++ src/index.js
@@ -1,25 +1,18 @@
+
 const menuBtnRef = document.querySelector('[data-menu-button]');
 const mobileMenuRef = document.querySelector('[data-menu]');
 const mobileMenuBodyRef = document.querySelector('[data-menu-is-open]');
 const menuCloseBtnRef = document.querySelector('[data-menu-close-button]');
-
-menuBtnRef.addEventListener('click', () => {
+const menuLinks = document.querySelectorAll('.header__item');
+function toggleMenu() {
   const expanded = menuBtnRef.getAttribute('aria-expanded') === 'true' || false;
-
   menuBtnRef.classList.toggle('is-open');
   menuBtnRef.setAttribute('aria-expanded', !expanded);
-
   mobileMenuRef.classList.toggle('is-open');
-
-  mobileMenuBodyRef.classList.toggle('menu-is-open');
-});
-menuCloseBtnRef.addEventListener('click', () => {
-  const expanded = menuBtnRef.getAttribute('aria-expanded') === 'true' || false;
-
-  menuBtnRef.classList.toggle('is-open');
-  menuBtnRef.setAttribute('aria-expanded', !expanded);
-
-  mobileMenuRef.classList.toggle('is-open');
-
-  mobileMenuBodyRef.classList.toggle('menu-is-open');
+  // mobileMenuBodyRef.classList.toggle('menu-is-open');
+}
+menuBtnRef.addEventListener('click', () => toggleMenu());
+menuCloseBtnRef.addEventListener('click', ()=> toggleMenu());
+menuLinks.forEach(item => {
+  item.addEventListener('click', ()=> toggleMenu());
 });
