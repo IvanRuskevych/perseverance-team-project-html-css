be4c473f8117fcf993bf2ab85af9257b5740e4bc
ivanruskevych <114290317+ivanruskevych@users.noreply.github.com>
2022-12-28 17:45:40 +0000

    Deploying to gh-pages from @ ivanruskevych/perseverance-team-project-html-css@8cedec789b887625d61ab863669a7f3c094e5f51 🚀

--- index.955aa665.js
+++
@@ -1,2 +0,0 @@
-new Swiper(".swiper",{direction:"horizontal",loop:!0,speed:400,spaceBetween:100,pagination:{el:".swiper-pagination",clickable:!0},autoplay:{delay:3e3}});
-//# sourceMappingURL=index.955aa665.js.map
--- index.fcd2e2b7.js
+++
@@ -1,2 +0,0 @@
-new Swiper(".swiper",{direction:"horizontal",loop:!0,speed:400,spaceBetween:100,pagination:{el:".swiper-pagination",clickable:!0},autoplay:{delay:3e3}});
-//# sourceMappingURL=index.fcd2e2b7.js.map
